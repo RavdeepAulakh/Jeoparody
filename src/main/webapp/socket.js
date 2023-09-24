3242933f09e7b3c44e1582503853d12ea9d78f75
Will Ondrik <willondrik@my.bcit.ca>
2023-09-24 16:39:25 -0700

    Socket is only initialized in multi-player

--- src/main/webapp/socket.js
+++ src/main/webapp/socket.js
@@ -1,23 +1,38 @@
 
-// Set up variables to insert the languageId/categoryId into the url string
 
-// Create a socketURL depending on the users language/category selection
-const socketURL = "ws://localhost:8081/demo_war_exploded/game";
-console.log(socketURL);
+// Select the multiplayer buton by ID
+let MP = document.getElementById('multiPlayer')
 
-// Establish a websocket connection to the server endpoint
-const webSocket = new WebSocket(socketURL);
 
-// Function for handling incoming WebSocket messages
-const socketFeed = document.getElementById('socket-feed');
-webSocket.onmessage = function(e) {
-    const message = e.data;
-    const socketItem = document.createElement('p');
+function initializeSocket() {
 
-    // Append the message to the new P element
-    socketItem.appendChild(document.createTextNode(message));
+    // Create a socketURL depending on the users language/category selection
+    // the '/game' at the end of the socketURL is the socket servlet endpoint
+    const socketURL = "ws://localhost:8081/demo_war_exploded/game";
+    console.log(socketURL);
 
-    // Append the p element with message text to the socket feed
-    //socketFeed.appendChild(socketItem);
-    socketFeed.insertBefore(socketItem, socketFeed.firstChild);
+    // Establish a websocket connection to the server endpoint
+    const webSocket = new WebSocket(socketURL);
+
+    // Function for handling incoming WebSocket messages
+    const socketFeed = document.getElementById('socket-feed');
+    webSocket.onmessage = function (e) {
+        const message = e.data;
+        const socketItem = document.createElement('p');
+
+        // Append the message to the new P element
+        socketItem.appendChild(document.createTextNode(message));
+
+        // Append the p element with message text to the socket feed
+        //socketFeed.appendChild(socketItem);
+        socketFeed.insertBefore(socketItem, socketFeed.firstChild);
+    }
 }
+
+
+
+// Add event listener for multiplayer button
+    // When triggered, it will start the socket
+MP.addEventListener('click', function() {
+    initializeSocket();
+});
